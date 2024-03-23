c84bb6aaad40cfd8eaa80d1a3be76e9c247b5cfd
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-03-24 00:56:38 +1100

    added validation for user selected mode

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ master_password = input("Enter your master password: ")
 
 mode = input(
     "would you like to add a new password or view an existing one (view/add): ").lower()
+
+if mode == "view":
+    pass
+elif mode == "add":
+    pass
+else:
+    print("Invalid option")
