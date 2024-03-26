9e5b49ef82f09245f82d4658d70f9ae798eaec87
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-03-27 01:38:30 +1100

    creater master key, added  add, view, load_key and write key functions

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
+from cryptography.fernet import Fernet
+
+"""
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+"""
+
+
+def load_key():
+    return open("key.key", "rb").read()
+
+
+key = load_key()
+
 master_password = input("Enter your master password: ")
 
-mode = input(
-    "would you like to add a new password or view an existing one (view/add): ").lower()
 
-if mode == "view":
-    pass
-elif mode == "add":
-    pass
-else:
-    print("Invalid option")
+def add():
+    name = input("What is the name of the account? ")
+    password = input("What is the password? ")
+
+    with open("passwords.txt", "a") as f:
+        f.write(name + " | " + password + "\n")
+
+
+def view():
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, password = data.split(" | ")
+            print("user: " + user + ", password: " + password)
+
+
+while True:
+    mode = input(
+        "would you like to add a new password or view an existing one (view/add/ q), press q to quit: ").lower()
+
+    if mode == "q":
+        break
+    elif mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid option")
+        continue
